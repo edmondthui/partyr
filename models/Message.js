6f27cc797306e9a03f9f3b1e791e37491b1698d6
jasminelok <jasminellok@gmail.com>
2020-12-01 02:16:35 -0500

    Add to Message schema

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const messageSchema = mongoose.Schema({
-  message: String,
-  username: String
+  message: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  partyId:{ 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "party"
+  }
 }, {
   timestamps: true,
 })
