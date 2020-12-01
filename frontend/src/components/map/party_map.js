c4ec9ae3f413d9e59ce68459cb89b94f24dff4cb
jasminelok <jasminellok@gmail.com>
2020-12-01 18:00:40 -0500

    Update map frontend component

--- frontend/src/components/map/party_map.js
+++ frontend/src/components/map/party_map.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router-dom';
 import './partymap.css';
 
 class PartyMap extends React.Component {
-
     render() {
       return (
         <div className="map-container" ref="map">
