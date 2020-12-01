7aa13ed37d8964f56286dff4473eb140f62883a1
Edmond Hui <edmond.t.hui@gmail.com>
2020-11-30 22:25:14 -0500

    prepare for heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
@@ -8,6 +9,13 @@ const users = require("./routes/api/users");
 const parties = require("./routes/api/parties");
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
