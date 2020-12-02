e408e9252989a6252f70a75b84575f78539e725f
Alexandria Wong <alexandriawhy@gmail.com>
2020-12-02 14:11:26 -0500

    Add geolocation access in party form

--- frontend/src/components/party/form/create_party_form.jsx
+++ frontend/src/components/party/form/create_party_form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+const keys = require('../../../config/keys')
 
 class CreatePartyForm extends React.Component {
   constructor(props) {
@@ -19,7 +20,8 @@ class CreatePartyForm extends React.Component {
   }
 
   componentWillMount() {
-    const mapOptions = {
+    const geolocation = navigator.geolocation;
+    let mapOptions = {
       center: { lat: 37.7758, lng: -122.435},
       zoom: 13
     }
@@ -34,17 +36,24 @@ class CreatePartyForm extends React.Component {
       height: '100%'
     }
 
-    this.map = <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle}></Map>
-    // this.map.addListener("click", (e) => {
-    //   let position = {lat: e.latLng.lat(), lng: e.latLng.lng()}
-    //   this.handleClick(position);
-    // })
+    if (geolocation) {
+      geolocation.getCurrentPosition(position => {
+        mapOptions = {
+          center: {lat: position.coords.latitude, lng: position.coords.longitude},
+          zoom: 13
+        }
+      this.setState({ map: <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle} onClick={this.handleClick}></Map> })
+      })
+    } else {
+      this.setState({ map: <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle} onClick={this.handleClick}></Map> })
+    }
+
   }
 
-  handleClick(position) {
+  handleClick(e, map, coord) {
     this.setState({
-      lat: position.lat,
-      lng: position.lng
+      lat: coord.latLng.lat(),
+      lng: coord.latLng.lng()
     })
   }
 
@@ -121,7 +130,7 @@ class CreatePartyForm extends React.Component {
           </div>
           <div className="location">
             <h3>Location:</h3>
-            {this.map}
+
             <label>Latitude:
               <input type="text" value={this.state.lat} onChange={this.update("lat")} disabled />
             </label>
@@ -132,9 +141,14 @@ class CreatePartyForm extends React.Component {
           </div>
           <button className="submit-btn">Create Party!</button>
         </form>
+        <div>
+          {this.state.map}
+        </div>
       </div>
     )
   }
 }
 
-export default withRouter(CreatePartyForm);
+export default GoogleApiWrapper({
+  apiKey: keys.mapsApiKey
+})(CreatePartyForm);
