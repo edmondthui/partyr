1bb9f1105fa595cbe7a05629217b8d6edfb1cca4
Edmond Hui <edmond.t.hui@gmail.com>
2020-12-02 14:49:40 -0500

    :sparkles: make map show current location

--- frontend/src/components/map/party_map.js
+++ frontend/src/components/map/party_map.js
@@ -6,11 +6,21 @@ import {Map, GoogleApiWrapper} from 'google-maps-react';
 const keys = require("../../config/keys.js")
 
 class PartyMap extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      map: null
+    }
+  }
   componentDidMount() {
-    const mapOptions = {
-      center: { lat: 37.7758, lng: -122.435 },
+
+    const geolocation = navigator.geolocation;
+
+    let mapOptions = {
+      center: { lat: 37.7758, lng: -122.435},
       zoom: 13
     }
+
     const mapStyles = {
       width: "500px",
       height: "500px",
@@ -22,12 +32,22 @@ class PartyMap extends React.Component {
       height: '100%'
     }
 
-    this.map = <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle}></Map>
+    if (geolocation) {
+      geolocation.getCurrentPosition(position => {
+        mapOptions = {
+          center: {lat: position.coords.latitude, lng: position.coords.longitude},
+          zoom: 13
+        }
+        this.setState({ map: <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle} onClick={this.handleClick}></Map> })
+      })
+    } else {
+      this.setState({ map: <Map google={this.props.google} initialCenter={mapOptions.center} zoom={mapOptions.zoom} style={mapStyles} containerStyle={containerStyle} onClick={this.handleClick}></Map> })
+    }
   }
   render() {
     return (
       <div className="map-container">
-        {this.map}
+        {this.state.map}
       </div>
     );
   }
