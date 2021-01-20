45fb8dfb6f9fbb9459fd0601304b3857f541710d
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-20 01:20:32 -0500

    Add index.scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import jwt_decode from "jwt-decode";
 import { setAuthToken } from "./util/session_api_util";
 import { logout } from "./actions/session_actions";
 import "./index.css";
+import "./index.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
