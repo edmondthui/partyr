b9126a2fae7dc3df335ba075693da6fcbeab67c4
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-20 02:11:15 -0500

    Isolate reset.scss and layout.scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import jwt_decode from "jwt-decode";
 
 import { setAuthToken } from "./util/session_api_util";
 import { logout } from "./actions/session_actions";
-import "./index.css";
 import "./index.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
